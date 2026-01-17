e38da54f218b6a8ec7f0453cf5c0a5ee2ad5d85d
s174086 <s174086@student.uken.krakow.pl>
2026-01-17 23:26:14 +0100

    Improve book update and add GET (by id) endpoint
    PUT now updates all fields
    added GET /books/(id) endpoint

--- app/routers/books.py
+++ app/routers/books.py
@@ -18,6 +18,12 @@ def get_db():
 def list_books(db: Session = Depends(get_db)):
     return db.query(models.Book).all()
 
+@router.get("/{book_id}", response_model=schemas.Book)
+def get_book(book_id: int, db: Session = Depends(get_db)):
+    db_book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    if not db_book:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return db_book
 
 @router.post("/", response_model=schemas.Book)
 def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
@@ -32,15 +38,21 @@ def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
     db.refresh(obj)
     return obj
 
-@router.put("/{book_id}")
+@router.put("/{book_id}", response_model=schemas.Book)
 def update_book(book_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)):
     db_book = db.query(models.Book).filter(models.Book.id == book_id).first()
     if not db_book:
         raise HTTPException(status_code=404, detail="Book not found")
+
     db_book.title = book.title
+    db_book.author = book.author
+    db_book.description = book.description
+    db_book.year = book.year
+
     db.commit()
     db.refresh(db_book)
     return db_book
+
 
 @router.delete("/{book_id}")
 def delete_book(book_id: int, db: Session = Depends(get_db)):
