bf7efd513c9499888cd7bc94d79d314fcb5e5be7
s174086 <s174086@student.uken.krakow.pl>
2026-01-18 20:57:33 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,8 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from typing import Generator
+from sqlalchemy.orm import Session
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dev.db")
 
@@ -14,3 +16,11 @@ else:
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
